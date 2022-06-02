18cf6eb66d8773f2d3b5b408af008a863b2c6459
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2022-06-02 16:57:50 -0300

    Com amor, Isaac

---
+++ hearts.js
@@ -0,0 +1,65 @@
+var brd = document.createElement("DIV");
+document.body.insertBefore(brd, document.querySelector("main"));
+
+const duration = 3000;
+const speed = 0.5;
+const cursorXOffset = 0;
+const cursorYOffset = -5;
+
+var hearts = [];
+		
+function generateHeart(x, y, xBound, xStart, scale)
+{
+	var heart = document.createElement("DIV");
+	heart.setAttribute('class', 'heart');
+	brd.appendChild(heart);
+	heart.time = duration;
+	heart.x = x;
+	heart.y = y;
+	heart.bound = xBound;
+	heart.direction = xStart;
+	heart.style.left = heart.x + "px";
+	heart.style.top = heart.y + "px";
+	heart.scale = scale;
+	heart.style.transform = "scale(" + scale + "," + scale + ")";
+	if(hearts == null)
+		hearts = [];
+	hearts.push(heart);
+	return heart;
+}
+
+var before = Date.now();
+var id = setInterval(frame, 5);
+var gr = setInterval(check, 100);
+
+function frame()
+{
+	var current = Date.now();
+	var deltaTime = current - before;
+	before = current;
+	for(i in hearts)
+	{
+		var heart = hearts[i];
+		heart.time -= deltaTime;
+		if(heart.time > 0)
+		{
+			heart.y -= speed;
+			heart.style.top = heart.y + "px";
+			heart.style.left = heart.x + heart.direction * heart.bound * Math.sin(heart.y * heart.scale / 30) / heart.y * 100 + "px";
+    }
+		else
+		{
+			heart.parentNode.removeChild(heart);
+			hearts.splice(i, 1);
+		}
+	}
+}
+
+function check()
+{
+    var start = 1 - Math.round(Math.random()) * 2;
+    var scale = Math.random() * Math.random() * 0.8 + 0.2;
+    var bound = 30 + Math.random() * 20;
+    const xPos = Math.random() * (window.innerWidth - 0) + 0;
+    generateHeart(xPos, window.innerHeight, bound, start, scale);
+}
